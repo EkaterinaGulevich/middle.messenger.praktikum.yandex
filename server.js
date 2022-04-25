c0de7b530ead064fe1db93d96908a37b8653f2b6
Ekaterina Gulevich <ekaterina07gulevich@yandex.ru>
2022-04-25 05:54:15 +0300

    fix express routing

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const PORT = 3000;
 
 app.use(express.static('./dist/'));
 
+app.get('/*', function(req, res){
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
+
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
 });
